9e216020cf6736ce260b46aaaa39194b0cecd777
Bastiche <bastien.velitchkine@live.fr>
2022-01-17 22:41:20 +0100

    [+] Started active contour workflow

--- lib/mask.py
+++ lib/mask.py
@@ -1,6 +1,9 @@
 import cv2
 import numpy as np
 from skimage.filters import threshold_otsu
+from skimage.color import rgb2gray
+from skimage.filters import gaussian
+from skimage.segmentation import active_contour
 
 
 def mask_rgb(img, thresholds):
@@ -56,6 +59,87 @@ def mask_otsu(img):
     return gray_img > thresh
 
 
+def init_contour(img, padding, nb_points):
+    '''
+    A function that initializes a contour for our dataset of images. 
+    They're all framed approximately the same way so it would be very inefficient to initialize the algorithm to a different contour for each image.
+    Since apples and tomatoes take almost all the space in the image, we will initialize a contour just a bit tighter than the borders of the images. 
+    We'll call the distance to the borders "padding".
+
+    Parameters
+    ----------
+    img : np.array
+        Image to mask.
+    padding: int
+        The distance from the borders of the picture for the contour.
+    nb_points: int
+        The number of points on each of our segments of the picture. With nb_points, we have a contour of 4 * nb_points points.
+
+    Returns
+    -------
+    contour: (np.array, np.array), dtype=np.uint8
+        The initial contour to run the active contour algorithm on our images.
+
+    Example
+    -------
+    If the img.shape == (w,h) and padding == n (provided that n < min(0.5*w, 0.5*h)) our contour has 4 segments:
+    | y = n and x in [n, w-n]
+    | x = w-n and y in [n, h-n]
+    | y = h-n and x in [w-n, n]
+    | x = n and y in [h-n, n]
+    '''
+    h, w = img[..., 0].shape
+    step_x = (w - 2 * padding) // nb_points
+    step_y = (h - 2 * padding) // nb_points
+
+    segment1_x = np.arange(padding, w-padding, step_x)
+    segment1_y = padding * np.ones(segment1_x.shape).astype(np.uint8)
+
+    segment2_y = np.arange(padding, h-padding, step_y)
+    segment2_x = (w-padding) * np.ones(segment2_y.shape).astype(np.uint8)
+
+    segment3_x = np.flip(segment1_x)
+    segment3_y = (h-padding) * np.ones(segment3_x.shape).astype(np.uint8)
+
+    segment4_y = np.flip(segment2_y)
+    segment4_x = padding * np.ones(segment4_y.shape).astype(np.uint8)
+
+    contour_x = np.concatenate(
+        (segment1_x, segment2_x, segment3_x, segment4_x), axis=0)
+    contour_y = np.concatenate(
+        (segment1_y, segment2_y, segment3_y, segment4_y), axis=0)
+
+    return np.array([contour_x, contour_y])
+
+
+def mask_active_contour(img, initial_contour):
+    '''
+    A function that computes the active contour of an image.
+    [TO DO] apply a mask on everything that's outside the perimeter defined by the contour.
+    '''
+    img = rgb2gray(img)
+    return active_contour(gaussian(img, 10, preserve_range=False),
+                          initial_contour, alpha=.001, beta=.01, w_line=.1, w_edge=-.1, gamma=.001)
+
+
+def test_active_contour(data):
+    '''
+    A function to compute and plot the active contour on an image of our dataset, to quickly assess the performances of the algorithm.
+    '''
+    _, ax = plt.subplots(figsize=(7, 7))
+    img = data[0]
+    initial_contour = init_contour(img, 25, 50)
+    snake = mask_active_contour(img, initial_contour)
+
+    ax.imshow(img, cmap=plt.cm.gray)
+    ax.plot(initial_contour[0], initial_contour[1], '--r', lw=1)
+    ax.plot(snake[0], snake[1], '--b', lw=4)
+    ax.set_xticks([]), ax.set_yticks([])
+    ax.axis([0, img.shape[1], img.shape[0], 0])
+
+    plt.show()
+
+
 def test_mask(data, mask_func, n=20):
     """Display n images with their masks (computed using mask_func)."""
     for i, img in enumerate(data):
@@ -76,6 +160,7 @@ def test_mask(data, mask_func, n=20):
 if __name__ == '__main__':
     from functools import partial
     from lib.data import get_dataset
+    import matplotlib.pyplot as plt
 
     infos = {'apple_a': (0, 100), 'apple_b': (1, 0), 'apple_c': (1, 0),
              'tomato': (1, 0)}
@@ -86,4 +171,5 @@ if __name__ == '__main__':
 
     # test_mask(data, mask_func, n=5)
     # test_mask(data, mask_apple_tomato, n=2)
-    test_mask(data, mask_otsu, n=2)
+    # test_mask(data, mask_otsu, n=2)
+    test_active_contour(data)
