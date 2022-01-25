b7ec02a6a24e27fffe9b3c0f248127d0441f84f7
Bastiche <bastien.velitchkine@live.fr>
2022-01-25 19:14:22 +0100

    [+] Aglomerative clustering for bag of words

--- lib/bag_of_words.py
+++ lib/bag_of_words.py
@@ -8,9 +8,26 @@ from lib.data import get_dataset
 from lib.descriptors import get_keypoints_sift
 from lib.mask import mask_rgb
 
+
 def bag_of_words_kmeans(descriptors, n_clusters, seed=0, save_path=None):
     kmeans = KMeans(n_clusters=n_clusters, random_state=seed).fit(descriptors)
     vocabulary = kmeans.cluster_centers_
+    if save_path is not None:
+        np.save(save_path, vocabulary)
+    return vocabulary
+
+
+def bag_of_words_agglomerative_clustering(descriptors, n_clusters, save_path=None):
+    agg_clustering = AgglomerativeClustering(
+        n_clusters=n_clusters).fit(descriptors)
+    labels = agg_clustering.labels_
+
+    # We had cluster centers in kmeans, in this case we'll take random descriptors in the clusters as vocabulary words
+    possible_labels = set(labels)
+    first_occurences_of_labels = [
+        np.where(labels == label)[0] for label in possible_labels]
+    vocabulary = np.array([descriptors[index]
+                          for index in first_occurences_of_labels])
     if save_path is not None:
         np.save(save_path, vocabulary)
     return vocabulary
@@ -26,7 +43,8 @@ def select_word(descriptors, vocabulary, dist, threshold=250_000_000):
             dist = np.empty((batch_size, n_desc, vocab_size))
             for i, desc_img in enumerate(descriptors):
                 desc_img = desc_img[:, ax, :]
-                dist[i] = np.sum(np.square(desc_img - vocabulary), axis=-1) ** 0.5
+                dist[i] = np.sum(
+                    np.square(desc_img - vocabulary), axis=-1) ** 0.5
         else:
             descriptors = descriptors[..., ax, :]
             vocabulary = vocabulary[ax, ax, ...]
@@ -48,7 +66,8 @@ def get_features(descriptors, vocabulary, dist='euclidian'):
     features_list = []
 
     for i in range(batch_size):
-        features = np.histogram(words[i], bins=len(vocabulary), density=True)[0]
+        features = np.histogram(
+            words[i], bins=len(vocabulary), density=True)[0]
         features_list.append(features * len(vocabulary))
     return np.array(features_list)
 
@@ -57,7 +76,7 @@ if __name__ == '__main__':
     n_clusters = 20
 
     infos = {'apple_a': (0, 20), 'apple_b': (1, 0), 'apple_c': (1, 0),
-                'tomato': (1, 0)}
+             'tomato': (1, 0)}
     (data_train, _), (data_val, _) = get_dataset('dataset/fruits', infos)
 
     mask_thresholds = {'r': (0.4, '>'), 'g': (0.5, '<'), 'b': (0.5, '<')}
@@ -79,7 +98,7 @@ if __name__ == '__main__':
     for i, descriptors in enumerate(desc_val):
         d = descriptors.shape[-1]
         descriptors = descriptors.reshape(1, -1, d)
-        plt.sca(axes[i//2, i%2])
+        plt.sca(axes[i//2, i % 2])
         plt.title('image {}'.format(i))
         plt.xlabel('word')
         plt.ylabel('proportion')
